e86886349cec740e88df6e6ac65a23647dce6202
juancho <juantognoli@gmail.com>
2024-09-29 15:06:40 -0300

    se modifica manera de gestionar DriverManager.initialize

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -12,23 +12,28 @@ import java.net.URL;
 public class DriverManager {
     private static AppiumDriver driver;
 
-    public static AppiumDriver initialize() throws MalformedURLException {
-        if (driver == null) {
-            // Configura las capacidades deseadas
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-            caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
-            caps.setCapability(MobileCapabilityType.UDID, "183a04f6908a");  // Usa adb devices para obtener el ID del dispositivo
+    public static AppiumDriver initialize() {
+        try {
+            if (driver == null) {
+                // Configura las capacidades deseadas
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+                caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+                caps.setCapability(MobileCapabilityType.UDID, "183a04f6908a");
 
-            // Usa las capacidades específicas de Android
-            caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
-            caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
+                // Capacidades específicas de Android
+                caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+                caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
 
-            // Motor de automatización
-            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                // Motor de automatización
+                caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
-            // Inicia el servidor de Appium y crea el driver
-            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+                // Inicia el servidor de Appium y crea el driver
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.out.println("La URL está mal formada: " + e.getMessage());
         }
         return driver;
     }

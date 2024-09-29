1867119f80887f6ba349162f81b2197870b26e8e
juancho <juantognoli@gmail.com>
2024-09-29 15:02:05 -0300

    Se mejora inicializacion del DriverManager y se elimina calculatorTest.java ya que se pasó a cucumber

--- src/test/java/stepDefinition/openCalculatorSteps.java
+++ src/test/java/stepDefinition/openCalculatorSteps.java
@@ -1,50 +1,28 @@
 package stepDefinition;
 
 import io.appium.java_client.AppiumBy;
-//import utils.DriverManager;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.DriverManager;
 
-import java.net.URL;
 import java.time.Duration;
-
 
 public class openCalculatorSteps {
 
     private AppiumDriver driver; // Variable de clase
 
     @Given("Abro la calculadora")
-    public void abro_la_calculadora() {
-        try {
-            // Configura las capacidades deseadas
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-            caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
-            caps.setCapability(MobileCapabilityType.UDID, "183a04f6908a");  // Usa adb devices para obtener el ID del dispositivo
-
-            // Usa las capacidades específicas de Android
-            caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator"); // Paquete de la calculadora
-            caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator"); // Actividad de la calculadora
-
-            // Motor de automatizacion
-            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-
-            // Inicia el servidor de Appium y crea el driver
-            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void abro_la_calculadora() throws Exception {
+        // Inicializa y asigna el driver a la variable de clase
+        driver = DriverManager.initialize();
     }
 
     @When("Ingreso un numero")
     public void ingresoUnNumero() {
+        // Utiliza el driver inicializado para interactuar con la aplicación
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         WebElement digit5 = wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.google.android.calculator:id/digit_5")));
         digit5.click();
--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -4,15 +4,13 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
-import io.cucumber.java.Scenario;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverManager {
-    private static AppiumDriver driver; // Cambiar MobileElement a WebElement
+    private static AppiumDriver driver;
 
     public static AppiumDriver initialize() throws MalformedURLException {
         if (driver == null) {
@@ -23,24 +21,23 @@ public class DriverManager {
             caps.setCapability(MobileCapabilityType.UDID, "183a04f6908a");  // Usa adb devices para obtener el ID del dispositivo
 
             // Usa las capacidades específicas de Android
-            caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator"); // Paquete de la calculadora
-            caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator"); // Actividad de la calculadora
+            caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+            caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
 
-            // Motor de automatizacion
+            // Motor de automatización
             caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
-
             // Inicia el servidor de Appium y crea el driver
-            AppiumDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
         }
         return driver;
     }
 
-    public static WebDriver getDriver() {
+    public static AppiumDriver getDriver() {
         return driver;
     }
 
-    public static void close(Scenario scenario) {
+    public static void close() {
         if (driver != null) {
             driver.quit();
             driver = null;
